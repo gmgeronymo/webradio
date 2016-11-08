18d908122b2167989eeab41d105904a66813eb2f
gmgeronymo <gean.geronymo@gmail.com>
2016-11-08 18:35:27 -0200

    teste 3 rotary

---
+++ rotary_test3.py
@@ -0,0 +1,16 @@
+import time
+import pigpio
+import rotary_encoder
+
+pos = 0
+
+def callback(way):
+    global pos
+    pos += way
+    print("pos={}".format(pos))
+xs
+pi = pigpio.pi()
+decoder = rotary_encoder.decoder(pi, 7, 8, callback)
+time.sleep(300)
+decoder.cancel()
+pi.stop()
